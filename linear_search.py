1705bc83106347d41c2b378a467c6403ccfc24b9
Taha Daneshmand <spy404.work@gmail.com>
2023-10-13 08:26:16 +0000

    Linear search - Python

---
+++ linear_search.py
@@ -0,0 +1,29 @@
+def linear_search(sequence: list, target: int) -> int:
+    for index, item in enumerate(sequence):
+        if item == target:
+            return index
+    return -1
+
+
+def rec_linear_search(sequence: list, low: int, high: int, target: int) -> int:
+    if not (0 <= high < len(sequence) and 0 <= low < len(sequence)):
+        raise Exception("Invalid upper or lower bound!")
+    if high < low:
+        return -1
+    if sequence[low] == target:
+        return low
+    if sequence[high] == target:
+        return high
+    return rec_linear_search(sequence, low + 1, high - 1, target)
+
+
+if __name__ == "__main__":
+    user_input = input("Enter numbers separated by comma:\n").strip()
+    sequence = [int(item.strip()) for item in user_input.split(",")]
+
+    target = int(input("Enter a single number to be found in the list:\n").strip())
+    result = linear_search(sequence, target)
+    if result != -1:
+        print(f"linear_search({sequence}, {target}) = {result}")
+    else:
+        print(f"{target} was not found in {sequence}")
