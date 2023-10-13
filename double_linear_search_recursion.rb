536a21ab490d6bea624c7d9ba8446909f99bdd2c
Taha Daneshmand <spy404.work@gmail.com>
2023-10-13 09:32:27 +0000

    Double linear search recursion - Ruby

--- double_linear_search_recursion.rb
+++ double_linear_search_recursion.rb
@@ -9,6 +9,7 @@ def recursive_double_linear_search(data, key, left = 0, right = 0)
   recursive_double_linear_search(data, key, left + 1, right - 1)
 end
 
+
 puts(recursive_double_linear_search([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], 5)) # => 5
 
 puts(recursive_double_linear_search([1, 2, 4, 5, 3], 4)) # => 2
